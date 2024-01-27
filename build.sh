83d38e70e883049dc1e607b6ffacdcb0285c53a4
guotaowei (A) <g00596867@china.huawei.com>
2024-01-27 14:49:57 +0800

    add lava adaptor directory && add Makefile

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@
 set -ex
 
 ocf_dir=$(cd $(dirname $0); pwd)
+
+cd ${ocf_dir}/lava_adaptor
+make distclean
+make -j16
---
+++ lava_adaptor/include/lava_adaptor.h
@@ -0,0 +1,5 @@
+/*
+ * Copyright(c) 2012-2021 Intel Corporation
+ * Copyright(c) 2024 Huawei Technologies
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
