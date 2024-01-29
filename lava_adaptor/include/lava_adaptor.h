40c8a85263f76bb4151597a8fd71ef5a3f33f382
guotaowei (A) <g00596867@china.huawei.com>
2024-01-29 11:00:00 +0800

    add the external structure and interface declaration

--- lava_adaptor/include/lava_adaptor.h
+++ lava_adaptor/include/lava_adaptor.h
@@ -3,3 +3,116 @@
  * Copyright(c) 2024 Huawei Technologies
  * SPDX-License-Identifier: BSD-3-Clause
  */
+
+#ifndef __LAVA_ADAPTOR_H__
+#define __LAVA_ADAPTOR_H__
+
+#include "ocf_cache_err.h"
+#include "ocf_cache_config.h"
+#include "ocf_cache_queue.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief ocf module initialization
+ *
+ * @param[in] cfg: ocf configuration parameters
+ *
+ * @retval STATE_SUCCESS when success, othewise STATE_FAIL
+ */
+int32_t ocf_init(struct ocf_config *cfg);
+
+/**
+ * @brief adding a core device corresponding to a slot
+ *
+ * @param[in] slot_id: unique id of a slot
+ *
+ * @retval STATE_SUCCESS when the core device is added successfully
+ *         STATE_CORE_EXIST when the core device corresponding to the slot already exists
+ *         othewise STATE_FAIL
+ */
+int32_t ocf_add_core(uint32_t slot_id);
+
+/**
+ * @brief removing a core device corresponding to a slot
+ *
+ * @param[in] slot_id: unique id of a slot
+ *
+ * @retval STATE_SUCCESS when the core device is removed successfully
+ *         STATE_CORE_NOT_EXIST when the core device corresponding to the slot doesn't exists
+ *         othewise STATE_FAIL
+ */
+int32_t ocf_remove_core(uint32_t slot_id);
+
+/**
+ * @brief clear the entire region data in the cache
+ *
+ * @param[in] ctx: user request context
+ *
+ * @retval STATE_SUCCESS when the invalid request is successfully submmitted
+ *         STATE_CORE_NOT_EXIST when the core device corresponding to the slot doesn't exists
+ *         othewise STATE_FAIL
+ */
+int32_t ocf_region_invalid(struct req_context *ctx);
+
+/**
+ * @brief clear the specified region segment data in the cache
+ *
+ * @param[in] ctx: user request context
+ *
+ * @retval STATE_SUCCESS when the invalid request is successfully submmitted
+ *         STATE_CORE_NOT_EXIST when the core device corresponding to the slot doesn't exists
+ *         othewise STATE_FAIL
+ */
+int32_t ocf_range_invalid(struct req_context *ctx);
+
+/**
+ * @brief check whether the specified segment data is hit in the cache
+ *
+ * @param[in] ctx: user request context
+ *
+ * @retval STATE_SUCCESS when the lookup request is successfully submmitted
+ *         STATE_CORE_NOT_EXIST when the core device corresponding to the slot doesn't exists
+ *         othewise STATE_FAIL
+ */
+int32_t ocf_lookup(struct req_context *ctx);
+
+/**
+ * @brief read the specified segment data from the cache
+ *
+ * @param[in] ctx: user request context
+ *
+ * @retval STATE_SUCCESS when the read request is successfully submmitted
+ *         STATE_CORE_NOT_EXIST when the core device corresponding to the slot doesn't exists
+ *         othewise STATE_FAIL
+ */
+int32_t ocf_get(struct req_context *ctx);
+
+/**
+ * @brief write the specified segment data to the cache
+ *
+ * @param[in] ctx: user request context
+ *
+ * @retval STATE_SUCCESS when the write request is successfully submmitted
+ *         STATE_CORE_NOT_EXIST when the core device corresponding to the slot doesn't exists
+ *         othewise STATE_FAIL
+ */
+int32_t ocf_put(struct req_context *ctx);
+
+/**
+ * @brief triggers processing of completion queue entries
+ *
+ * @param[in] io_worker_id: unique id of the io worker, which is used to index the io worker submission queue
+ * @param[in] max_num: maximum number of cq entries per time
+ *
+ * @retval STATE_FAIL when the cq of the io worker doesn't exist
+ *         othewise STATE_SUCCESS
+ */
+int32_t ocf_poll(uint32_t io_worker_id, uint32_t max_num);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ lava_adaptor/include/ocf_cache_queue.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright(c) 2012-2021 Intel Corporation
+ * Copyright(c) 2024 Huawei Technologies
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __OCF_CACHE_QUEUE_H__
+#define __OCF_CACHE_QUEUE_H__
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct req_context {
+	void *req_identifier;
+	/*!< pointer to request context */
+
+	uint32_t io_worker_id;
+
+	uint32_t slot_id;
+
+	uint64_t region_id;
+
+	uint64_t offset;
+
+	uint64_t len;
+
+	char *buffer;
+
+	int (*cb)(int32_t ret, void *ctx);
+	/*!< request completion callback; ctx pointer to req_context */
+};
+
+#ifdef __cplusplus
+}
+#endif
+#endif
