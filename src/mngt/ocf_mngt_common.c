33b3fffc8bdaae8b0dc5c378cd1a4274d3332079
Neo <2549951418@qq.com>
2024-03-20 15:54:50 +0800

    优化删除slot的计时

--- src/mngt/ocf_mngt_common.c
+++ src/mngt/ocf_mngt_common.c
@@ -100,7 +100,7 @@ void cache_mngt_core_deinit_attached_meta(ocf_core_t core)
 		if (curr_cline == cache->device->collision_table_entries)
 			hash++;
 		else
-			env_msleep(100);
+			env_msleep(10);
 	}
 }
 
