8d7f0c7e5c093f580d427241da5a3b5c9fd89449
g00596867 <guotaowei4@huawei.com>
2024-06-04 20:53:07 +0800

    cache chunk use independent thread

--- lava_ocf_adaptor/src/chunk.cpp
+++ lava_ocf_adaptor/src/chunk.cpp
@@ -9,16 +9,21 @@
 #include <linux/fs.h>
 #include <libaio.h>
 #include <pthread.h>
+#include <queue>
 #include "chunk.h"
 #include "log.h"
 #include "ocf_env.h"
 #include "ocf_adaptor_err.h"
+
+using namespace std;
 
 #define AIO_REQUEST_ERR 1
 #define CHUNK_SIZE (1 << 27)	// 128MiB
 #define NETWORK_DELAY_US 10
 #define SEC2USEC 1000000
 #define IO_SUBMIT_POLL 5
+#define QUEUE_NUM_LIMIT 4096
+#define SUBMIT_NUM_LIMIT 128 // 1M / cache_line_size
 
 #define AIO_DEPTH 0x80000
 
@@ -28,32 +33,17 @@ typedef struct {
 	uint32_t size;
 	bool allocated;			/* chunk是否已分配 */
 } Chunk;					/* chunk对接磁盘 */
-
 typedef Chunk *Chunk_t;
 
-static int g_disk_fd;
-static uint64_t g_chunk_num;			/* chunk总数 */
-static Chunk_t g_chunks = nullptr;
-static pthread_mutex_t g_chunk_mutex;	/* 申请chunk时加锁 */
-static io_context_t g_ctx;
-
-/* 系统调度可能导致普通sleep类函数睡眠时间不准确 */
-static inline void SpinSleepUS(uint64_t sleepTime)
-{
-	struct timeval startTime, stopTime;
-	uint64_t start, end;
-
-	gettimeofday(&startTime, nullptr);
-	start = startTime.tv_sec * SEC2USEC + startTime.tv_usec;
-	do {
-		gettimeofday(&stopTime, nullptr);
-		end = stopTime.tv_sec * SEC2USEC + stopTime.tv_usec;
-	} while (end - start < sleepTime);
-}
+typedef struct {
+    int op;
+    Request_t req;
+} OpCtx;
+typedef OpCtx *OpCtx_t;
 
 class RequestCounter {
 public:
-	RequestCounter(Request *req, int max) : req_(req), max_(max), error_(0)
+	RequestCounter(Request_t req, int max) : req_(req), max_(max), error_(0)
 	{
 		env_atomic_set(&curr_, 0);
 	}
@@ -69,15 +59,16 @@ public:
 	}
 
 public:
-	Request *req_; /* 原始的Request */
+	Request_t req_; /* 原始的Request */
 	int max_;      /* 一个Request拆分出的异步AioRequest的数量 */
 	env_atomic curr_;
 	int error_;
 };
+typedef RequestCounter *RequestCounter_t;
 
 class AioRequest {
 public:
-	AioRequest(Segment *segment, std::shared_ptr<RequestCounter> counter)
+	AioRequest(Segment_t segment, RequestCounter_t counter)
 		: segment_(segment), counter_(counter)
 	{}
 
@@ -91,9 +82,45 @@ public:
 	virtual ~AioRequest() {}
 
 public:
-	Segment *segment_;
-	std::shared_ptr<RequestCounter> counter_;
+	Segment_t segment_;
+	RequestCounter_t counter_;
 };
+typedef AioRequest *AioRequest_t;
+
+static int g_disk_fd;
+static uint64_t g_chunk_num;			/* chunk总数 */
+static Chunk_t g_chunks = nullptr;
+static pthread_mutex_t g_chunk_mutex;	/* 申请chunk时加锁 */
+static io_context_t g_ctx;
+static pthread_t t1;
+static pthread_t t2;
+static int stop;
+static pthread_spinlock_t sq_lock;
+static pthread_spinlock_t cq_lock;
+static struct iocb *ic[SUBMIT_NUM_LIMIT];
+static struct iocb ics[SUBMIT_NUM_LIMIT];
+static struct io_event ie[QUEUE_NUM_LIMIT];
+static OpCtx sv[QUEUE_NUM_LIMIT];
+static queue<OpCtx> sq;
+static queue<RequestCounter_t> cq;
+
+/* 系统调度可能导致普通sleep类函数睡眠时间不准确 */
+static inline void SpinSleepUS(uint64_t sleepTime)
+{
+	struct timeval startTime, stopTime;
+	uint64_t start, end;
+
+	gettimeofday(&startTime, nullptr);
+	start = startTime.tv_sec * SEC2USEC + startTime.tv_usec;
+	do {
+		gettimeofday(&stopTime, nullptr);
+		end = stopTime.tv_sec * SEC2USEC + stopTime.tv_usec;
+	} while (end - start < sleepTime);
+}
+
+static void* aio_process(void *arg);
+
+static void* aio_poll(void *arg);
 
 extern "C" int InitChunkPool(const char *disk_path)
 {
@@ -122,6 +149,10 @@ extern "C" int InitChunkPool(const char *disk_path)
 		return -1;
 	}
 
+	for (int i = 0; i < SUBMIT_NUM_LIMIT; ++i) {
+		ic[i] = &ics[i];
+	}
+
 	g_chunks = new Chunk[g_chunk_num];
 	if (unlikely(!g_chunks)) {
 		ocf_adaptor_log(OCF_LOG_ERROR, "chunk create fail\n");
@@ -137,12 +168,22 @@ extern "C" int InitChunkPool(const char *disk_path)
 		g_chunks[i].allocated = false;
 	}
 	pthread_mutex_init(&g_chunk_mutex, nullptr);
+	pthread_spin_init(&sq_lock, 0);
+	pthread_spin_init(&cq_lock, 0);
+	stop = false;
+	pthread_create(&t1, NULL, aio_process, nullptr);
+	pthread_create(&t2, NULL, aio_poll, nullptr);
 	return 0;
 }
 
 extern "C" void DeInitChunkPool()
 {
 	if (g_chunks) {
+		stop = true;
+		pthread_join(t1, NULL);
+		pthread_join(t2, NULL);
+		pthread_spin_destroy(&sq_lock);
+		pthread_spin_destroy(&cq_lock);
 		delete[] g_chunks;
 		g_chunks = nullptr;
 		pthread_mutex_destroy(&g_chunk_mutex);
@@ -185,42 +226,59 @@ int FreeChunks(std::vector<uint64_t> *chunk_ids)
 	return 0;
 }
 
-int SubmitWrite(uint64_t chunk_id, Segment *seg, std::shared_ptr<RequestCounter> counter)
+static int Push(int op, Request_t req)
 {
-	int ret;
-	struct iocb iocbpp;
-	struct iocb *p = &iocbpp;
-	AioRequest *req = new AioRequest(seg, counter);
-	uint64_t offset = chunk_id * CHUNK_SIZE + seg->offset;
-	io_prep_pwrite(p, g_disk_fd, seg->data, seg->length, offset);
-	iocbpp.data = req;
-	do {
-		ret = io_submit(g_ctx, 1, &p);
-		if (ret == -EAGAIN) {
-			PollChunkCompletion(IO_SUBMIT_POLL);
-		}
-	} while (ret == -EAGAIN);
-
-	return 0;
+    pthread_spin_lock(&sq_lock);
+    if (sq.size() >= QUEUE_NUM_LIMIT) {
+        pthread_spin_unlock(&sq_lock);
+        return -1;
+    }
+    sq.push({op, req});
+    pthread_spin_unlock(&sq_lock);
+    return 0;
 }
 
-int SubmitRead(uint64_t chunk_id, Segment *seg, std::shared_ptr<RequestCounter> counter)
+static int Submit(int op, Request_t req)
 {
-	int ret;
-	struct iocb iocbpp;
-	struct iocb *p = &iocbpp;
-	AioRequest *req = new AioRequest(seg, counter);
-	uint64_t offset = chunk_id * CHUNK_SIZE + seg->offset;
-	io_prep_pread(p, g_disk_fd, seg->data, seg->length, offset);
-	iocbpp.data = req;
-	do {
-		ret = io_submit(g_ctx, 1, &p);
-		if (ret == -EAGAIN) {
-			PollChunkCompletion(IO_SUBMIT_POLL);
-		}
-	} while (ret == -EAGAIN);
+    int ret;
+    struct iocb **ic_now;
 
-	return 0;
+    int size = req->segments.size();
+    uint64_t chunk_id = req->chunk_id;
+    RequestCounter_t counter = new RequestCounter(req, size);
+
+    if (op == 0) {
+        for (int i = 0; i < size; ++i) {
+            Segment *seg = &req->segments[i];
+            AioRequest_t a = new AioRequest(seg, counter);
+            uint64_t offset = chunk_id * CHUNK_SIZE + seg->offset;
+            io_prep_pread(ic[i], g_disk_fd, seg->data, seg->length, offset);
+            ic[i]->data = a;
+        }
+    } else {
+        for (int i = 0; i < size; ++i) {
+            Segment *seg = &req->segments[i];
+            AioRequest_t a = new AioRequest(seg, counter);
+            uint64_t offset = chunk_id * CHUNK_SIZE + seg->offset;
+            io_prep_pwrite(ic[i], g_disk_fd, seg->data, seg->length, offset);
+            ic[i]->data = a;
+        }
+    }
+
+    ic_now = ic;
+    while (size > 0) {
+        ret = io_submit(g_ctx, size, ic_now);
+        if (ret >= 0) {
+            size -= ret;
+            ic_now += ret;
+        } else if (ret == -EAGAIN) {
+            ocf_adaptor_log(OCF_LOG_ERROR, "io_submit again\n");
+        } else {
+            ocf_adaptor_log(OCF_LOG_ERROR, "io_submit fail, ret %d\n", ret);
+        }
+    }
+
+    return 0;
 }
 
 int AioWrite(Request_t req)
@@ -230,13 +288,7 @@ int AioWrite(Request_t req)
 	}
 
 	SpinSleepUS(NETWORK_DELAY_US);		/* sleep for network delay mock */
-
-	int size = req->segments.size();
-	std::shared_ptr<RequestCounter> counter = std::make_shared<RequestCounter>(req, size);
-	for (int i = 0; i < size; ++i) {
-		SubmitWrite(req->chunk_id, &req->segments[i], counter);
-	}
-	return 0;
+	return Push(1, req);
 }
 
 int AioRead(Request_t req)
@@ -246,30 +298,77 @@ int AioRead(Request_t req)
 	}
 
 	SpinSleepUS(NETWORK_DELAY_US);		/* sleep for network delay mock */
-
-	int size = req->segments.size();
-	std::shared_ptr<RequestCounter> counter = std::make_shared<RequestCounter>(req, size);
-	for (int i = 0; i < size; ++i) {
-		SubmitRead(req->chunk_id, &req->segments[i], counter);
-	}
-	return 0;
+	return Push(0, req);
 }
 
-int PollChunkCompletion(uint32_t max)
+int PollChunkCompletion(uint32_t max_nr)
 {
-	int request_complete_cnt = 0; /* 记录上层下发的Request完成数量，Request里的所有segment都处理完毕 */
-	struct io_event *events = new io_event[max];
-	int num_events = io_getevents(g_ctx, 0, max, events, nullptr);
-	for (int i = 0; i < num_events; ++i) {
-		struct io_event *event = &events[i];
-		AioRequest *req = static_cast<AioRequest *>(event->data);
-		if (req->Complete(event->res)) {
-			request_complete_cnt++;
-			SpinSleepUS(NETWORK_DELAY_US);	/* sleep for network delay mock */
-			req->counter_->Callback();
-		}
-		delete req;
+	int n;
+    RequestCounter_t *cv = new RequestCounter_t[max_nr];
+	if (unlikely(cv == nullptr)) {
+		return 0;
 	}
-	delete[] events;
-	return request_complete_cnt;
+    pthread_spin_lock(&cq_lock);
+    n = (max_nr < cq.size() ? max_nr : cq.size());
+    for (int i = 0; i < n; ++i) {
+        cv[i] = cq.front();
+        cq.pop();
+    }
+    pthread_spin_unlock(&cq_lock);
+
+    for (int i = 0; i < n; ++i) {
+        RequestCounter_t counter = cv[i];
+        counter->Callback();
+        delete counter;
+    }
+    delete []cv;
+	return n;
+}
+
+static void* aio_process(void *arg)
+{
+	int n;
+	while (!(stop)) {
+		pthread_spin_lock(&sq_lock);
+		n = ((QUEUE_NUM_LIMIT < sq.size()) ? QUEUE_NUM_LIMIT : sq.size());
+		for (int i = 0; i < n; ++i) {
+			sv[i] = sq.front();
+			sq.pop();
+		}
+		pthread_spin_unlock(&sq_lock);
+
+		for (int i = 0; i < n; ++i) {
+			Submit(sv[i].op, sv[i].req);
+		}
+
+		if (n == 0) {
+			usleep(10);
+		}
+	}
+
+	pthread_exit(0);
+}
+
+static void* aio_poll(void *arg)
+{
+	int n;
+	while (!(stop)) {
+		n = io_getevents(g_ctx, 0, QUEUE_NUM_LIMIT, ie, nullptr);
+		for (int i = 0; i < n; ++i) {
+			struct io_event *event = &ie[i];
+			AioRequest_t req = static_cast<AioRequest_t>(event->data);
+			if (req->Complete(event->res)) {
+				pthread_spin_lock(&cq_lock);
+	            cq.push(req->counter_);
+	            pthread_spin_unlock(&cq_lock);
+			}
+			delete req;
+		}
+
+		if (n == 0) {
+			usleep(10);
+		}
+	}
+
+	pthread_exit(0);
 }
